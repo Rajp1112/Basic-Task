4c90287fab0b869726a4317dbca75e91b58ffc4b
Resum <talvaniyar@gmail.com>
2024-11-07 16:53:14 +0530

    change all

--- login_signup_page/login_signup.js
+++ login_signup_page/login_signup.js
@@ -1,14 +1,14 @@
-let signup = document.querySelector(".signup");
-let login = document.querySelector(".login");
-let slider = document.querySelector(".slider");
-let formSection = document.querySelector(".form-section");
+let signup = document.querySelector(".signup2");
+let login = document.querySelector(".login2");
+let slider = document.querySelector(".slider2");
+let formSection = document.querySelector(".form-section2");
 
 signup.addEventListener("click", () => {
-    slider.classList.add("moveslider");
-    formSection.classList.add("form-section-move");
+    slider.classList.add("moveslider2");
+    formSection.classList.add("form-section-move2");
 });
 
 login.addEventListener("click", () => {
-    slider.classList.remove("moveslider");
-    formSection.classList.remove("form-section-move");
+    slider.classList.remove("moveslider2");
+    formSection.classList.remove("form-section-move2");
 });
