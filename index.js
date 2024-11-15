71ad55de51bdc24a2a8c1d80a281f0def8eb7fc3
Resum <talvaniyar@gmail.com>
2024-11-15 16:10:12 +0530

    change all

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const login =()=>{
-    
-}
+const counteNumber = document.querySelectorAll('.counteNumber');
+
+const speed = 3;
+
+counteNumber.forEach((curElm)=>{
+
+    const updateNumber = ()=>{
+        const targetNumber = parseInt(curElm.dataset.number);
+        // console.log(targetNumber);
+        const initialData = parseInt(curElm.innerText );
+        // console.log(initialData);
+        
+        const incrementData = Math.trunc(targetNumber/speed);
+        console.log(incrementData);
+
+        if (initialData < targetNumber) {
+            curElm.innerText= `${initialData + incrementData}k+`;
+            setTimeout(updateNumber,50);
+        }
+        
+    }
+
+
+    updateNumber();
+})
